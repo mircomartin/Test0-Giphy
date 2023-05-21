c7d85b7373a4fd7fb68e8084890872a2cf0c324a
Mir Martin <mirmartin@MacBook-Pro-de-Mir.local>
2023-05-21 18:15:28 -0300

    cambios

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useForm, type SubmitHandler } from 'react-hook-form'
 import { UserContext } from '../context/UserContext'
